8c01b35cad5d0fdc1eabe9b6fdc1ce9166ad21e4
DaveRLashley <d.lashley.strategy@gmail.com>
2025-06-20 11:40:07 -0400

    fix: wrap app in QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-// src/main.tsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AnalyticsProvider } from "@/context/AnalyticsContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const root = createRoot(document.getElementById("root")!);
+const queryClient = new QueryClient();
 
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AnalyticsProvider>
-      <App />
-    </AnalyticsProvider>
+    <QueryClientProvider client={queryClient}>
+      <AnalyticsProvider>
+        <App />
+      </AnalyticsProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
